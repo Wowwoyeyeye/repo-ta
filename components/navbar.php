04a991e1c17c6b9f94772206ab4d5c66e3d778b3
Fahmi Nabil mMaulana <fahminabilmaulana.com>
2024-06-01 20:21:27 +0700

    "hapus file php"

--- components/navbar.php
+++
@@ -1,9 +0,0 @@
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Contact Me</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Login</a></li>
-                    <!-- <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li> -->
-                </ul>
-            </div>
